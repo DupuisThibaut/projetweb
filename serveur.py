9e429a67e8447b1230c1d533a6c80f693557aa76
DupuisThibaut <73234193+DupuisThibaut@users.noreply.github.com>
2023-01-17 12:41:52 +0100

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -1,24 +1,32 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import sqlite3 as sql
 
 from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
 login={}
 
-@app.route('/dashboard/<name>')
-def dashboard(name):
-   return 'welcome %s' % name
-
-@app.route('/login',methods = ['POST', 'GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-   if request.method == 'POST':
-      user = request.form['name']
-      return redirect(url_for('dashboard',name = user))
-   else:
-      user = request.args.get('name')
-      return render_template('login.html')
+  #https://pythonbasics.org/flask-sqlite/
+    error = None
+    if request.method == 'POST':
+      test=sql.connect('database.db')
+      cursor=test.cursor()
+      iden=request.form['username']
+      cursor.execute("SELECT mdp FROM user WHERE id=?", (iden))
+      response = cursor.fetchone()
+      password=request.form['password']
+      if response[0]==password:
+          test.close()
+      else:
+          error = 'identifiant ou mot de passe incorrect'
+          test.close()
+          return render_template('login.html', error=error)
+    test.close()
+    return render_template('index.html')
 
 @app.route('/')
 @app.route('/index')
@@ -29,37 +37,27 @@ def index():
 def mesQcm():
     return render_template("qcm.html")
 
-@app.route('/creerCompte')
+@app.route('/creerCompte', methods=['POST', 'GET'])
 def creerCompte():
-    return render_template("creerCompte.html")
+  #https://pythonbasics.org/flask-sqlite/
+    msg = None
+    if request.method == 'POST':
+      try:
+        iden=request.form['username']
+        mdp=request.form['password']
+        with sql.connect('database.db') as con :
+          cur=con.cursor()
+          cur.execute("INSERT INTO user (id,mdp) VALUES (?,?)",(iden,mdp))
+          con.commit()
+          msg="Compte crée"
+      except:
+        con.rollback()
+        msg="erreur"
+      finally:
+        return redirect('http://localhost:8888/')
+        con.close()
+    return render_template('creerCompte.html')
 
-@app.route('/login/<identifiant>/<mdp>')
-def login2(identifiant,mdp):
-    if login[identifiant]==mdp:
-        return render_template("index.html")
-    else: render_template(login.html)
-
-@app.route('/creerCompte/<identifiant>/<mdp>')
-def creerCompte2(identifiant,mdp):
-    login[identifiant]=mdp
-    return render_template("index.html")
-
-@app.route('/hello')
-def hello_world():
-   return "hello world"
-
-@app.route('/product/<name>')
-def get_product(name):
-  return "The product is " + str(name)
-
-@app.route('/sale/<transaction_id>')
-def get_sale(transaction_id=0):
-  return "The transaction is "+str(transaction_id)
-
-@app.route('/create/<first_name>/<last_name>')
-def create(first_name=None, last_name=None):
-  return 'Hello ' + first_name + ',' + last_name
-
-app.run(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=8888)
 if __name__ == '__main__':
    app.run(debug = True)
